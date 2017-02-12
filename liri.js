94bc900d2d01698ba1252b9ed89984f2ac9f8cba
Justin Bellero <jpbellero@gmail.com>
2017-02-11 23:07:15 -0500

    adds movies search

--- liri.js
+++ liri.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-const tweet = require("./services/tweet.lock.js");
 const spotify = require("./services/spot.js");
-// const imdb = require("./services/imdb.js");
+const movies = require("./services/movies.js");
 
 const args = process.argv.slice(2);
 
-switch (args[0]){
-  case "my-tweets":
-    tweet.getTweets(args[1]);
-    break;
-  case "spotify-this":
-    spotify(args.slice(1).toString());
-    break;
+switch (args[0]) {
+    case "spotify-this":
+        spotify(args.slice(1).join(' '));
+        break;
+    case "movie-this":
+        movies("The Calling")
+        break;
 }
